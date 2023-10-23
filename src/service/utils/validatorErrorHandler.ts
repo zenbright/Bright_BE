f7e90f69056c5522d6134eac5745504dfbb15efb
Mudoker <huuquoc7603@gmail.com>
2023-10-23 17:01:17 +0700

    fix file name

---
+++ src/service/utils/validatorErrorHandler.ts
@@ -0,0 +1,20 @@
+import { validationResult } from 'express-validator';
+import logger from '../../logger';
+
+export default function validatorErrorHandler(req: any, res: any, next: any) {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        const arrayErrors = errors.array();
+
+        // Log errors
+        logger.error('Validation Errors:', arrayErrors);
+
+        return res.status(422).json({
+            success: false,
+            errors: arrayErrors,
+        });
+    }
+
+    return next();
+}
