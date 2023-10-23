15d28a89a6f1d59d8df7eea9ebf376170ab88fbc
Mudoker <95454901+Mudoker@users.noreply.github.com>
2023-10-23 16:44:56 +0700

    Delete build directory

--- build/service/redis/redisConfig.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from 'redis';
-const options = {
-    port: 6379,
-    host: '127.0.0.1'
-};
-const redisClient = createClient(options);
-redisClient.on('error', (err) => console.log('Redis Client Error', err));
-redisClient.on('connect', () => console.log('Redis connected'));
-export default redisClient;
