cab88469bc81eef2d600ae3af843ba462bbc1e93
Quoc Doan <95454901+Mudoker@users.noreply.github.com>
2023-10-31 08:34:13 +0700

    Update gitAuth.route.ts - remove error handler call

--- src/service/authentication/github/gitAuth.route.ts
+++ src/service/authentication/github/gitAuth.route.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import * as gitAuthController from './gitAuth.controller';
 import * as IPSpamChecker from '../../middleware/api.limiter';
 import * as APIValidator from '../../middleware/api.validator';
-import ErrorHandler from '../../utils/errorResponseHandler';
+
 const router = Router();
 
 // Login with GitHub
@@ -10,7 +10,6 @@ router.post('/git',
     IPSpamChecker.checkIpSpamServer('/auth/git'), // Check IP spam
     APIValidator.loginWithGitHubValidator, // Validate request body
     gitAuthController.loginWithGitHub, // Handle request
-    ErrorHandler // Handle error response
 );
 
 export default router;
