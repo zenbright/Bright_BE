33d6457676a1c4e9d3ef209dd993d7cb0db0176f
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2023-11-04 10:45:27 +0700

    structure searchUser folder

---
+++ src/service/authentication/bright/searchUser/searchUser.service.ts
@@ -0,0 +1,44 @@
+import userCredentials from "../../../../models/userCredentials";
+import userInformation from "../../../../models/userInfo";
+
+export async function searchUserService(req: any, res: any, next: any) {
+  try {
+    const { account, fullname } = req.body;
+
+    if (!account || !fullname) {
+      return res
+        .status(400)
+        .json({ error: "Invalid User Account or Fullname." });
+    }
+
+    // Find user credentials with account
+    const userCred = await userCredentials.findOne({
+      account: account,
+    });
+
+    if (userCred) {
+      // Find user Info with id
+      const userInfo = await userInformation.findOne({ _id: userCred.userId });
+
+      if (userInfo) {
+        if (userInfo.fullname == fullname) {
+          return res.json(userInfo, userCred); // return userInfo and userCredentials
+        } else {
+          res.status(400).json({
+            message: "Invalid User Fullname.",
+          });
+        }
+      } else {
+        res.status(404).json({
+          message: "User Information not found.",
+        });
+      }
+    } else {
+      res.status(404).json({
+        message: "User credentials not found.",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+}
