e14ee3e6e299a6ac2148759fb50f04f328ffa3d3
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2023-11-06 15:49:28 +0700

    service structure

--- src/service/authentication/bright/manageImage/getImage/getImage.service.ts
+++ src/service/authentication/bright/manageImage/getImage/getImage.service.ts
@@ -1,10 +1,33 @@
-import axios from 'axios';
-import userCredentials from '../../../../../models/userCredentials';
-import userInfo from '../../../../../models/userInfo';
-import mongoose from 'mongoose';
-import * as Formatter from '../../../../utils/formatter';
+import userInformation from "../../../../../models/userInfo";
 
 export async function getImageService(req: any, res: any, next: any) {
-   
-       
+  try {
+    const { userInfoId } = req.body;
+    // Find the existing account with id
+    const userInfo = await userInformation.findOne({
+      _id: userInfoId,
+    });
+
+    if (!userInfo) {
+      return res.status(404).json({ error: "User account not found." });
+    } else {
+      console.log("found Info");
+      // TODO: return it as a file
+      const binary = userInfo.profileImage;
+      const image = convertBinaryToImage(binary);
+
+      return res.status(200).json({
+        image: image,
+        message: "Got the profile image successfully",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+function convertBinaryToImage(binary: String | undefined) {
+  // Convert binary to base 64
+  // Convert the base64 to string (use toString())
+  // return that imagePath string
 }
--- src/service/authentication/bright/manageImage/saveImage/saveImage.service.ts
+++ src/service/authentication/bright/manageImage/saveImage/saveImage.service.ts
@@ -1,10 +1,38 @@
-import axios from 'axios';
-import userCredentials from '../../../../../models/userCredentials';
-import userInfo from '../../../../../models/userInfo';
-import mongoose from 'mongoose';
-import * as Formatter from '../../../../utils/formatter';
+import userInformation from "../../../../../models/userInfo";
+import fs from "fs";
 
 export async function saveImageService(req: any, res: any, next: any) {
-   
-       
+  try {
+    const { imagePath, userInfoId } = req.body;
+    const base64Image = convertImageToBinary(imagePath);
+
+    // Find the existing account with id
+    const userInfo = await userInformation.findOne({
+      _id: userInfoId,
+    });
+
+    if (!userInfo) {
+      return res.status(404).json({ error: "User account not found." });
+    } else {
+      console.log("found Info");
+      // Update the profileImage as base64 string
+      userInfo.profileImage = base64Image;
+
+      await userInfo.save();
+      return res
+        .status(200)
+        .json({ message: "Profile Image updated successfully" });
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+function convertImageToBinary(imagePath: any) {
+  // Read the image file
+  const imageBuffer = fs.readFileSync(imagePath);
+  // Convert the image data to Base64 binary format
+  const base64Image = imageBuffer.toString("base64");
+
+  return base64Image;
 }
