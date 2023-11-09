07bbc32ca5c2a784ec1fe4d75f0f50fa3253b8fe
Mudoker <huuquoc7603@gmail.com>
2023-11-09 17:38:16 +0700

    update constraint

--- src/service/user/passwordChange/passwordChange.service.ts
+++ src/service/user/passwordChange/passwordChange.service.ts
@@ -1,9 +1,15 @@
 import userCredentials from "../../../models/userCredentials";
+import {ERROR_CODE} from "../../utils/constants";
 
 export async function passwordChangeService(req: any, res: any, next: any) {
   try {
     const { account, provider, newPassword } = req.body;
 
+    // Only allow password change with bright accounts
+    if (provider != 'bright') {
+      return res.status(500).json({ error: ERROR_CODE.INSUFFICIENT_PERMISSION });
+    }
+    
     // Find the existing credential with account
     const userCred = await userCredentials.findOne({
       account: account,
