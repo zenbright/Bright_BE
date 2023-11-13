20894736034b08e2a4555612b25063d1ad0619cc
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2023-11-13 11:48:44 +0700

    identify received msg & sender & total client count

--- src/app.ts
+++ src/app.ts
@@ -149,17 +149,27 @@ const server = app.listen(PORT_SERVER, () => {
 
 // Connect to socket.io
 const io = new Server(server, {});
+let socketsConnected = new Set()
 
 io.on("connection", (socket: Socket) => {
   console.log("a user connected");
   console.log("socket id: " + socket.id);
+  // Increase Total Client Count
+  socketsConnected.add(socket.id)
+  io.emit('clients-total', socketsConnected.size) 
 
-  socket.on("chat message", (msg: string) => {
-    io.emit("chat message", msg);
+  socket.on('message', (data) => {
+    console.log("received message: "  + data.message);
+    console.log("sender: "  + socket.id);
+    io.emit('message', data);
+    socket.broadcast.emit('chat-message', data);
   });
 
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+  socket.on('disconnect', () => {
+    console.log("a user disconnected");
+    // Decrease Total Client Count
+    socketsConnected.delete(socket.id)
+    io.emit('clients-total', socketsConnected.size) 
   });
 });
 
