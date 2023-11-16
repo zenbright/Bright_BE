1e33003308353a53f60a29a1e37252ae1b9f96be
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2023-11-16 09:59:56 +0700

    static route fix using userId and groupId

--- src/static.route.ts
+++ src/static.route.ts
@@ -1,23 +1,61 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
+import userCredentials from "./models/userCredentials";
+import Group from "./models/group";
 
 const staticRoutes = express.Router();
 const __dirname = path.resolve();
 
-staticRoutes.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/service/authentication/github/index.html'));
+staticRoutes.get("/", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "src/service/authentication/github/index.html"),
+  );
 });
 
-staticRoutes.get('/realtimeChat.css', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/service/user/realtimeChat/realtimeChat-FE/realtimeChat.css'));
+staticRoutes.get(
+  "/:userId/:groupId/realtimeChat.css",
+  (req, res) => {
+    res.sendFile(
+      path.join(
+        __dirname,
+        "src/service/user/realtimeChat/realtimeChat-FE/realtimeChat.css",
+      ),
+    );
+  },
+);
+
+staticRoutes.get("/:userId/:groupId/main.js", (req, res) => {
+  res.sendFile(
+    path.join(
+      __dirname,
+      "src/service/user/realtimeChat/realtimeChat-FE/main.js",
+    ),
+  );
 });
 
-staticRoutes.get('/main.js', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/service/user/realtimeChat/realtimeChat-FE/main.js'));
+staticRoutes.get("/:userId/:groupId/realtimechat", async (req, res) => {
+  validateUserGroupId(req, res);
+
+  res.sendFile(
+    path.join(
+      __dirname,
+      "src/service/user/realtimeChat/realtimeChat-FE/realtimeChat.html",
+    ),
+  );
 });
 
-staticRoutes.get('/realtimeChat', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/service/user/realtimeChat/realtimeChat-FE/realtimeChat.html'));
-});
+async function validateUserGroupId(req: any, res: any) {
+  const { userId, groupId } = req.params;
+  try {
+    const user = await userCredentials.findOne({ userId: userId });
+    const group = await Group.findOne({ groupId: groupId });
+
+    if (!user || !group) {
+      return res.status(404).send("User or group not found");
+    }
+  } catch (error) {
+    return res.status(500).send({ error: error });
+  }
+}
 
 export default staticRoutes;
