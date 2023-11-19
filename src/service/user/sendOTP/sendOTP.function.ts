a58b9d59049c5904089b2c756e184466b21f12e1
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2023-11-19 15:33:01 +0700

    time change

--- src/service/user/sendOTP/sendOTP.function.ts
+++ src/service/user/sendOTP/sendOTP.function.ts
@@ -2,13 +2,14 @@ import nodemailer from "nodemailer";
 import OTPverification from "../../../models/OTPverification";
 import { AUTH_EMAIL, AUTH_PASSWORD } from "../../../config";
 
+const expirationTimeInMinutes = 60; // OTP expires in 60 minutes
+
 export function generateOTP() {
   // a random 4-digit OTP
   return Math.floor(1000 + Math.random() * 9000).toString();
 }
 
 export async function saveOTPMemory(OTP: string, userId: any) {
-  const expirationTimeInMinutes = 60; // OTP expires in 60 minutes
   const expirationTimeInMilliseconds = expirationTimeInMinutes * 60 * 1000;
 
   const newOTPverification = new OTPverification({
@@ -45,6 +46,6 @@ export function generateEmailContent(email: string, OTP: string) {
     from: AUTH_EMAIL,
     to: email,
     subject: "Bright OTP Verification Code",
-    html: `<p>Enter <b>${OTP}</b> in the app to verify your email. This code <b>expires in 15 minutes.</b></p>`,
+    html: `<p>Enter <b>${OTP}</b> in the app to verify your email. This code <b>expires in ${expirationTimeInMinutes} minutes.</b></p>`,
   };
 }
