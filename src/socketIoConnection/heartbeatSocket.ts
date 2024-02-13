fbc5547b477e84b05910dfe33c89f7b2876b4fc9
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-02-13 14:28:34 +0700

    feat: add updateUserState logic

--- src/socketIoConnection/heartbeatSocket.ts
+++ src/socketIoConnection/heartbeatSocket.ts
@@ -14,14 +14,18 @@ export const initHeartbeatSocket = (server: any) => {
     if (referer) {
       const userId = referer.split("/")[3];
       console.log(userId, " socket connected");
+      updateUserState(userId, true);
 
-      socket.on("heartbeat",  () => {
-        console.log(userId, " heartbeat checked at " , new Date().toISOString());
+      socket.on("heartbeat", () => {
+        console.log(userId, " heartbeat checked at ", new Date().toISOString());
       });
 
       socket.on("disconnect", () => {
         console.log(userId, " socket disconnected");
+        updateUserState(userId, false);
       });
     }
   });
 };
+
+function updateUserState(userId: string, isOnline: boolean) {}
