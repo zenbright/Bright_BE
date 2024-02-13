48cd204cc26a3e2c48c4c38e80e9a995973b3473
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-02-13 15:06:31 +0700

    feat!: updateUserState

--- src/socketIoConnection/heartbeatSocket.ts
+++ src/socketIoConnection/heartbeatSocket.ts
@@ -1,4 +1,5 @@
 import { Server } from "socket.io";
+import userInfoModel from "../models/userInfoModel";
 
 const heartbeatIo = new Server();
 
@@ -7,25 +8,37 @@ export const initHeartbeatSocket = (server: any) => {
     path: "/heartbeat",
   });
 
-  heartbeatIo.on("connection", (socket: any) => {
+  heartbeatIo.on("connection", async (socket: any) => {
     const referer = socket.handshake.headers.referer;
 
     // Extract userId and groupId from the referer URL
     if (referer) {
       const userId = referer.split("/")[3];
       console.log(userId, " socket connected");
-      updateUserState(userId, true);
+      await updateUserState(userId, true);
 
       socket.on("heartbeat", () => {
         console.log(userId, " heartbeat checked at ", new Date().toISOString());
       });
 
-      socket.on("disconnect", () => {
+      socket.on("disconnect", async () => {
         console.log(userId, " socket disconnected");
-        updateUserState(userId, false);
+        await updateUserState(userId, false);
       });
     }
   });
 };
 
-function updateUserState(userId: string, isOnline: boolean) {}
+async function updateUserState(userId: string, isOnline: boolean) {
+  const userInfo = await userInfoModel.findOne({ _id: userId });
+  if (!userInfo) {
+    console.log("User not found");
+  } else {
+    userInfo.isOnline = isOnline;
+    if (!isOnline) {
+      userInfo.lastOnlineTime = new Date();
+    }
+    userInfo.save();
+    console.log("User online info updated");
+  }
+}
