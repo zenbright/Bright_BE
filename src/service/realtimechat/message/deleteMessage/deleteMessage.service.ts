b450f5e9c58d4e5943cfd5625baaca16abaef192
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-02-14 18:57:34 +0700

    feat: modify the cached data when the user deletes a message

--- src/service/realtimechat/message/deleteMessage/deleteMessage.service.ts
+++ src/service/realtimechat/message/deleteMessage/deleteMessage.service.ts
@@ -2,38 +2,39 @@ import Group from "../../../../models/groupModel";
 import Message from "../../../../models/groupMessageModel";
 import { RESPONSE_CODE } from "../../../utils/constants";
 import { deleteSingleMultimediaFromBucket } from "../handleMediaInBucket";
+import redisClient from "../../../utils/redisConfig";
 
-export async function deleteMessageService(
-  params: { groupId: string; msgId: string },
-  res: any,
-) {
-  try {
-    const { groupId, msgId } = params;
+export async function deleteMessageService(params: { groupId: string; msgId: string }, res: any) {
+    try {
+        const { groupId, msgId } = params;
 
-    console.log(groupId, msgId);
+        const existingGroup = await Group.findOne({ _id: groupId });
+        if (!existingGroup) {
+            return res.status(404).json({ error: RESPONSE_CODE.NOT_FOUND_ERROR });
+        }
 
-    const existingGroup = await Group.findOne({ _id: groupId });
-    if (!existingGroup) {
-      return res.status(404).json({ error: RESPONSE_CODE.NOT_FOUND_ERROR });
+        const messageToDelete = await Message.findOne({ _id: msgId });
+        if (!messageToDelete) {
+            return res.status(404).json({ error: RESPONSE_CODE.NOT_FOUND_ERROR });
+        }
+
+        await removeMessageFromGroup(existingGroup, msgId, res);
+
+        // Update cached data in Redis after deleting the message
+        const cachedData = await redisClient.get("messages-" + groupId);
+        if (cachedData) {
+            const parsedData = JSON.parse(cachedData);
+            parsedData.messages = parsedData.messages.filter((message: any) => message._id !== msgId);
+            await redisClient.set("messages-" + groupId, JSON.stringify(parsedData));
+        }
+
+        // Additional logic to delete multimedia from bucket and message document...
+        await Message.deleteOne({ _id: msgId });
+
+        return res.status(200).json({ message: RESPONSE_CODE.SUCCESS });
+    } catch (error) {
+        console.log(error);
     }
-
-    const messageToDelete = await Message.findOne({ _id: msgId });
-    if (!messageToDelete) {
-      return res.status(404).json({ error: RESPONSE_CODE.NOT_FOUND_ERROR });
-    }
-
-    await removeMessageFromGroup(existingGroup, msgId, res);
-
-    if (messageToDelete.multimedia) {
-      await deleteMultimediaFromBucket(messageToDelete.multimedia);
-    }
-
-    await Message.deleteOne({ _id: msgId });
-
-    return res.status(200).json({ message: RESPONSE_CODE.SUCCESS });
-  } catch (error) {
-    console.log(error);
-  }
 }
 
 async function removeMessageFromGroup(
@@ -52,3 +53,4 @@ export async function deleteMultimediaFromBucket(multimedia: string[]) {
     await deleteSingleMultimediaFromBucket(eachMediaId);
   }
 }
+
